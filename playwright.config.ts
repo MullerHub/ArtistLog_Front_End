2c660175e3c85f62b4935874af404491da44ffba
Murilo Muller (Mac) <murilomuller@protonmail.com>
2026-03-04 03:01:52 -0300

    [playwright.config.ts] Isolated commit: playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -42,8 +42,9 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run dev',
+    command: `NEXT_PUBLIC_API_URL=${process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8080'} npm run dev:turbo`,
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
+    timeout: 120000,
   },
 })
