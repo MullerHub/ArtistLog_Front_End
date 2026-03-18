9cf047de0f9199ea9460e5c11317903758b95793
Murilo Muller (Mac) <murilomuller@protonmail.com>
2026-03-18 03:34:01 -0300

    feat(i18n): update i18n index configuration

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,8 +2,6 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import ptBR from "./locales/pt-BR.json";
-import en from "./locales/en.json";
-import es from "./locales/es.json";
 
 i18n
   .use(LanguageDetector)
@@ -11,9 +9,8 @@ i18n
   .init({
     resources: {
       "pt-BR": { translation: ptBR },
-      en: { translation: en },
-      es: { translation: es },
     },
+    supportedLngs: ["pt-BR"],
     fallbackLng: "pt-BR",
     interpolation: { escapeValue: false },
     detection: {
